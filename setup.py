b40d519f61ea00a97eb12b0cb4f5fbc4f9fbae0f
yazbekw <yazbek965@gmail.com>
2025-07-01 14:12:06 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="crypto-bot",
+    version="0.1",
+    install_requires=[
+        'ccxt==4.2.85',
+        'pandas==1.5.3',
+        'numpy==1.23.5',
+        'TA-Lib==0.4.28; sys_platform != "win32"',
+        'ta==0.11.0; sys_platform == "win32"',
+        'requests==2.31.0',
+        'APScheduler==3.10.1',
+        'python-telegram-bot==20.6',
+        'python-dotenv==1.0.0'
+    ]
+)
