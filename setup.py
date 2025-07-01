c74bbeb5751cfbe94dd5e56533df5ee7bf286199
yazbekw <yazbek965@gmail.com>
2025-07-01 16:20:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(
         'ccxt==4.2.85',
         'pandas==1.5.3',
         'numpy==1.23.5',
-        'ta==0.11.0',  # استخدام ta لجميع الأنظمة
+        'ta==0.11.0',  # تم استخدام ta بدلاً من TA-Lib
         'requests==2.31.0',
         'APScheduler==3.10.1',
         'python-telegram-bot==20.6',
-        'python-dotenv==1.0.0'
+        'python-dotenv==1.0.0',
+        'setuptools>=68.0.0'
     ],
     setup_requires=['setuptools>=68.0.0']
 )
